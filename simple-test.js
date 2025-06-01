de6a87d146150c9077531745bc579cb8af77c5b4
YongChenSu <yongchensu0@gmail.com>
2025-06-01 12:03:28 +0800

    feat: remove Puppeteer launch arguments for testing on github action

--- simple-test.js
+++ simple-test.js
@@ -3,14 +3,8 @@ const puppeteer = require('puppeteer');
 const assert = require('assert');
 
 (async () => {
-  // 1. 啟動瀏覽器（非 headless 模式）
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage'
-    ]
   });
   const page = await browser.newPage();
 
